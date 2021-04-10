9ce9576ede4153d2fe8b6439169f37cf90dc2349
VCU- Chapagain <78453389+Nikitachapagai@users.noreply.github.com>
2021-04-09 21:58:36 -0400

    Update homework.py

--- spiral/homework.py
+++ spiral/homework.py
@@ -2,3 +2,6 @@ def spiralize(number):
     mid_point = (number - 1) / 2
     diagonal = (3 + 2 * mid_point * (8 * mid_point * mid_point + 15 * mid_point + 13)) / 3
     return int(diagonal)
+
+
+print(spiralize(501))
